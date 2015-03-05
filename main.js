64793e3bd92cbc0475d295a3ab056f8ae8933d82
adamhosier <hosieradam@gmail.com>
2015-03-05 14:24:30 +0000

    scrolls to reference on click

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var FADE_TIME = 100;
-var SCROLL_TIME = 200;
+var SCROLL_TIME = 0;
 var HEADER_OFFSET = 150; //pixels
+const REF_BOX_FADE_TIME = 200;
 
 $(document).ready(function(){
 
@@ -36,6 +37,24 @@ $(document).ready(function(){
   //initialise navigator
   updateNavigator();
 
+  //Manage reference text, click & hover events
+  $("sup").each(function() {
+    $(this).html('<a>[' + this.id[1] + ']</a>');
+  }).hover(function(e){
+    $("body").append(makeReferenceBox(this.id, e.pageX, e.pageY));
+    $(".ref-box").fadeIn(REF_BOX_FADE_TIME);
+  }, function(){
+    $(".ref-box").fadeOut(REF_BOX_FADE_TIME, function () {
+      $(this).remove();
+    });
+  }).click(function(){
+    $("li").removeClass("selected");
+    var element = getReferenceById(this.id)
+    element.addClass("selected")
+    $("html, body").animate({
+      scrollTop: element.offset().top - HEADER_OFFSET
+    }, SCROLL_TIME)
+  })
 
 });
 
@@ -58,3 +77,16 @@ function updateNavigator() {
     }, SCROLL_TIME)
   });
 }
+
+function getReferenceById(id) {
+  return $("ol li:nth-of-type(" + id[1] + "):visible");
+}
+
+function makeReferenceBox(id, x, y) {
+  return $("<div></div>", {
+    "class": "ref-box",
+    style: "left:" + (x + 5) + "px; top:" + (y + 5) + "px",
+    html: "<h4>Reference ID: " + id[1] + " (click for more)</h4><hr>" + getReferenceById(id).text()
+  });
+}
+
